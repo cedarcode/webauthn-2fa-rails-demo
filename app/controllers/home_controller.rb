7795196694a251a4d327fd7f0fa19d17b6cdba84
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2020-05-06 12:39:11 -0300

    feat: list user's security key when present

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,11 @@ class HomeController < ApplicationController
 
   def index
   end
+
+  private
+
+  def credential
+    @credential ||= current_user.webauthn_credentials.first
+  end
+  helper_method :credential
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   has_many :webauthn_credentials, dependent: :destroy
 
   validates :username, presence: true, uniqueness: true
+
+  def second_factor_enabled?
+    webauthn_credentials.any?
+  end
 end
