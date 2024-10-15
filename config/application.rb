ff9a0cbde648be140b8d3be1beee411ce1ae2e65
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2024-10-15 14:06:26 -0300

    Use Rails 7.2 defaults

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module Webauthn2faRailsDemo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.2
 
     # Configuration for the application, engines, and railties goes here.
     #
