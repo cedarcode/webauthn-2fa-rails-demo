5166b935266cd1feebc5227181940e39e5b4d80c
Nicolas Temciuc <nicolas.temciuc@cedarcode.com>
2025-03-18 15:37:51 -0300

    chore: remove `config.active_job.verbose_enqueue_logs` config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,9 +41,6 @@ Rails.application.configure do
   # Append comments with runtime information tags to SQL queries in logs.
   config.active_record.query_log_tags_enabled = true
 
-  # Highlight code that enqueued background job in logs.
-  config.active_job.verbose_enqueue_logs = true
-
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
